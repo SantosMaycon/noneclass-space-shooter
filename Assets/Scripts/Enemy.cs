8e59480c7c54d22bc34004f5c0caaa8c0b33c16b
SantosMaycon <mayconalves.santos14@gmail.com>
2022-12-09 22:35:48 -0200

    add collison between player and enemies

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -34,4 +34,11 @@ public class Enemy : MonoBehaviour {
       Destroy(gameObject);
     }
   }
+
+  private void OnCollisionEnter2D(Collision2D other) {
+    if (other.gameObject.CompareTag("Nave")) {
+      this.damegedLife(life);
+      other.gameObject.GetComponent<PlayerController>().damegedLife(1);
+    }
+  }
 }
